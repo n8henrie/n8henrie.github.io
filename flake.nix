56df7483802f3c8fc341cdfe7335d245792624da
Nathan Henrie <nate@n8henrie.com>
2025-10-18 15:25:38 -0600

    cleanup

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   # https://pages.github.com/versions.json
   # https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=ruby
   inputs.nixpkgs.url = "github:nixos/nixpkgs/ab7b6889ae9d484eed2876868209e33eb262511d";
-  # inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   outputs =
     { nixpkgs, ... }:
     let
@@ -30,26 +29,34 @@
               ruby = jekyll_ruby;
               name = "n8henrie.com";
               gemdir = ./.;
-              gemConfig.nokogiri = attrs: {
-                buildInputs = [ pkgs.zlib ];
-                buildFlags = [
-                  "--with-iconv-dir=${libiconv}"
-                  "--with-opt-include=${libiconv}/include"
-                ];
-              };
-              gemConfig.ffi = attrs: {
-                buildInputs = [ pkgs.libffi ];
+              gemConfig = {
+                nokogiri = attrs: {
+                  buildInputs = [ pkgs.zlib ];
+                  buildFlags = [
+                    "--with-iconv-dir=${libiconv}"
+                    "--with-opt-include=${libiconv}/include"
+                  ];
+                };
+                ffi = attrs: {
+                  buildInputs = [ pkgs.libffi ];
+                };
               };
             })
-            # iconv
-            # libffi
             pkg-config
             jekyll_ruby
           ];
-          # shellHook = ''
-          #   export LANG="en_US.UTF-8"
-          #   make develop
-          # '';
+          shellHook = ''
+              export LANG="en_US.UTF-8"
+            	trap "kill 0" EXIT
+            	bundle exec jekyll clean
+            	bundle exec guard -i &
+            	DISABLE_WHITELIST=true bundle exec jekyll serve --config _config.yml,_config_dev.yml --incremental --watch --drafts &
+            	until [[ "$(curl --silent --write-out "%{http_code}" --output /dev/null http://localhost:4000)" -eq 200 ]]; do
+                echo "Waiting for jekyll server..."
+                sleep 1
+              done
+              open "http://localhost:4000"
+          '';
         };
       }
     );
